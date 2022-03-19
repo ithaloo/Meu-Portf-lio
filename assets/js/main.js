d3688e095c328956caafd9c4cadef1bfdf88affd
ithaloo <italo482010@hotmail.com>
2022-03-19 14:15:39 -0300

    estruturando o JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,18 +39,17 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 function toggleSkills(){
   let itemClass = this.parentNode.className
 
-  for(i = 0; i < skillsContent.lenght; i++){
-    skillsContent[i].className = 'skills__content skills__close'
+  for(i = 0; i < skillsContent.length; i++){
+      skillsContent[i].className = 'skills__content skills__close'
   }
   if(itemClass === 'skills__content skills__close'){
-    this.parentNode.className = 'skills__content skills__open'
+     this.parentNode.className = 'skills__content skills__open'
   }
 }
-  
+
 skillsHeader.forEach((el) =>{
   el.addEventListener('click', toggleSkills)
 })
-
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
@@ -59,12 +58,12 @@ tabs.forEach(tab =>{
   tab.addEventListener('click', () =>{
     const target= document.querySelector(tab.dataset.target)
     
-    tabContents.forEach(tabContents =>{
-      tabContents.classList.remove('qualification__active')
+    tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('qualification__active')
     } )
     target.classList.add('qualification__active') 
 
-    tab.forEach(tab =>{
+    tabs.forEach(tab =>{
       tab.classList.remove('qualification__active')
     })
     tab.classList.add('qualification__active')
